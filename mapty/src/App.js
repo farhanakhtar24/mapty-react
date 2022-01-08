ba518685bfdcab8ef3ea2082d3112c4ae2aea18e
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-01-09 02:16:54 +0530

    configured

--- mapty/src/App.js
+++ mapty/src/App.js
@@ -1,11 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Fragment } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       Hello
-    </div>
+    </Fragment>
+    // <MapContainer center={ [51.505, -0.09] } zoom={ 13 } scrollWheelZoom={ false }>
+    //   <TileLayer
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    //   <Marker position={ [51.505, -0.09] }>
+    //     <Popup>
+    //       A pretty CSS3 popup. <br /> Easily customizable.
+    //     </Popup>
+    //   </Marker>
+    // </MapContainer>
   );
 }
 
